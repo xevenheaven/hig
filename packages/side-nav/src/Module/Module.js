3350c91840a3b6ef033bfe470307bb3d0a0ac191
Morris Allison III <morris.allison@autodesk.com>
2018-08-24 13:51:11 -0700

    fix(side-nav-module): correct controlled functionality

--- packages/side-nav/src/Module/Module.js
+++ packages/side-nav/src/Module/Module.js
@@ -14,12 +14,18 @@ export default class Module extends Component {
     icon: PropTypes.node,
     /** URL to navigate to when clicking this module */
     link: PropTypes.string,
+    /** Default uncontrolled state of `minimized` */
+    defaultMinimized: PropTypes.bool,
     /** Indicates whether to display nested submodules */
     minimized: PropTypes.bool,
     /** Called when clicking on the collapse button */
     onClickCollapseButton: PropTypes.func,
     /** Called when clicking on the title */
     onClickTitle: PropTypes.func,
+    /** Called when the submodules are visible */
+    onExpand: PropTypes.func,
+    /** Called when the submodules are hidden */
+    onMinimize: PropTypes.func,
     /** Called when hovering over the title */
     onMouseOver: PropTypes.func,
     /** A label for rendering this Module */
@@ -29,57 +35,92 @@ export default class Module extends Component {
   };
 
   static defaultProps = {
-    minimized: false
+    defaultMinimized: false
   };
 
-  constructor(props) {
-    super(props);
+  state = {
+    minimized: this.props.defaultMinimized
+  };
 
-    this.state = {
-      minimized: props.minimized
+  /**
+   * @param {Function} callback
+   */
+  setMinimize(minimized, callback) {
+    const { onExpand, onMinimize } = this.props;
+
+    if (minimized && onMinimize) {
+      onMinimize();
+    } else if (!minimized && onExpand) {
+      onExpand();
+    }
+
+    this.setState({ minimized }, callback);
+  }
+
+  isMinimizedControlled() {
+    return this.props.minimized !== undefined;
+  }
+
+  isMinimized() {
+    return this.isMinimizedControlled()
+      ? this.props.minimized
+      : this.state.minimized;
+  }
+
+  /**
+   * @param {Function} callback
+   */
+  toggleMinimize(callback) {
+    this.setMinimize(!this.state.minimized, callback);
+  }
+
+  /**
+   * @param {function(): function} getHandler
+   * @returns {function(MouseEvent): void}
+   */
+  createMinimizeToggler(getHandler) {
+    return () => {
+      const handler = getHandler();
+
+      if (this.isMinimizedControlled()) {
+        handler();
+      } else {
+        this.toggleMinimize(handler);
+      }
     };
   }
 
   /**
-   * @param {Function} callback, function to call after minimize state has been toggled
-   * @returns {Function}
+   * @param {MouseEvent} event
    */
-  createMinimizeToggler = callback => () => {
-    this.setState(
-      { minimized: !this.state.minimized },
-      () => callback && callback()
-    );
-  };
+  handleClickCollapseButton = this.createMinimizeToggler(
+    () => this.props.onClickCollapseButton
+  );
 
   /**
-   * @returns {PresenterBag}
+   * @param {MouseEvent} event
    */
-  createPresenterBag() {
-    const { minimized } = this.state;
-
-    return {
-      minimized,
-      onClickCollapseButton: this.createMinimizeToggler(
-        this.props.onClickCollapseButton
-      ),
-      onClickTitle: this.createMinimizeToggler(this.props.onClickTitle)
-    };
-  }
-
-  /**
-   * @param {PresenterBag}
-   */
-  renderPresenter = presenterBag => {
-    const presenterProps = { ...this.props, ...presenterBag };
-    const { children, ...otherProps } = presenterProps;
-
-    return <ModulePresenter {...otherProps}>{children}</ModulePresenter>;
-  };
+  handleClickTitle = this.createMinimizeToggler(() => this.props.onClickTitle);
 
   render() {
-    const presenterBag = this.createPresenterBag();
-    const { renderPresenter } = this;
+    const {
+      children,
+      onClickCollapseButton,
+      onClickTitle,
+      ...otherProps
+    } = this.props;
+    const minimized = this.isMinimized();
+    const { handleClickCollapseButton, handleClickTitle } = this;
 
-    return renderPresenter(presenterBag);
+    return (
+      <ModulePresenter
+        minimized={minimized}
+        onClickCollapseButton={handleClickCollapseButton}
+        onClickTitle={handleClickTitle}
+        {...otherProps}
+      >
+        {children}
+      </ModulePresenter>
+    );
   }
 }
--- packages/side-nav/src/Module/Module.test.js
+++ packages/side-nav/src/Module/Module.test.js
@@ -5,9 +5,21 @@ import { mount } from "enzyme";
 import Module from "./Module";
 
 describe("side-nav/Module", () => {
+  const onExpand = jest.fn();
+  const onMinimize = jest.fn();
+  const onClickTitle = jest.fn();
+  const onClickCollapseButton = jest.fn();
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  function getCollapseButton(wrapper) {
+    return wrapper.find("CollapseButton");
+  }
+
   describe("onClickTitle", () => {
     it("adds click handler to title", () => {
-      const onClickTitle = jest.fn();
       const wrapper = mount(
         <Module title="Module" onClickTitle={onClickTitle} />
       );
@@ -19,15 +31,83 @@ describe("side-nav/Module", () => {
 
   describe("onClickCollapseButton", () => {
     it("adds click handler to collapse button", () => {
-      const onClickCollapseButton = jest.fn();
       const wrapper = mount(
         <Module title="Module" onClickCollapseButton={onClickCollapseButton}>
           Content
         </Module>
       );
 
-      wrapper.find(".hig__side-nav__module__collapse-button").simulate("click");
+      const collapseButton = getCollapseButton(wrapper);
+
+      collapseButton.simulate("click");
       expect(onClickCollapseButton).toHaveBeenCalled();
+    });
+  });
+
+  describe("onMinimize", () => {
+    it("is called when the Module is minimized", () => {
+      const wrapper = mount(
+        <Module title="Module" defaultMinimized onMinimize={onMinimize}>
+          Content
+        </Module>
+      );
+
+      const collapseButton = getCollapseButton(wrapper);
+
+      expect(onMinimize).not.toHaveBeenCalled();
+      collapseButton.simulate("click");
+      expect(onMinimize).not.toHaveBeenCalled();
+      collapseButton.simulate("click");
+      expect(onMinimize).toHaveBeenCalledTimes(1);
+      collapseButton.simulate("click");
+      expect(onMinimize).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe("onExpand", () => {
+    it("is called when the Module is expanded", () => {
+      const wrapper = mount(
+        <Module title="Module" defaultMinimized onExpand={onExpand}>
+          Content
+        </Module>
+      );
+
+      const collapseButton = getCollapseButton(wrapper);
+
+      expect(onExpand).not.toHaveBeenCalled();
+      collapseButton.simulate("click");
+      expect(onExpand).toHaveBeenCalledTimes(1);
+      collapseButton.simulate("click");
+      expect(onExpand).toHaveBeenCalledTimes(1);
+      collapseButton.simulate("click");
+      expect(onExpand).toHaveBeenCalledTimes(2);
+    });
+  });
+
+  describe("minimized", () => {
+    it("prevents default behavior", () => {
+      const wrapper = mount(
+        <Module
+          title="Module"
+          minimized
+          onClickCollapseButton={onClickCollapseButton}
+          onClickTitle={onClickTitle}
+          onExpand={onExpand}
+          onMinimize={onMinimize}
+        >
+          Content
+        </Module>
+      );
+
+      const collapseButton = getCollapseButton(wrapper);
+
+      collapseButton.simulate("click");
+      collapseButton.simulate("click");
+      collapseButton.simulate("click");
+
+      expect(onClickCollapseButton).toHaveBeenCalledTimes(3);
+      expect(onExpand).not.toHaveBeenCalled();
+      expect(onMinimize).not.toHaveBeenCalled();
     });
   });
 
