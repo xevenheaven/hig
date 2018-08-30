cba9347ca285bafebeb52bb6aed2d3f85f2efaaf
Ari Russo <ari.russo@autodesk.com>
2018-08-30 11:49:24 -0400

    test: Add gemini snapshots for clicked label and label with reference to form field

--- packages/label/src/__gemini__/Label.gemini.js
+++ packages/label/src/__gemini__/Label.gemini.js
@@ -1,6 +1,22 @@
-gemini.suite("Label", suite => {
-  suite
-    .setUrl("iframe.html?selectedKind=Label&selectedStory=default")
-    .setCaptureElements(".storybook-component")
-    .capture("Default");
+gemini.suite("Label", () => {
+  gemini.suite("default", suite => {
+    suite
+      .setUrl("iframe.html?selectedKind=Label&selectedStory=default")
+      .setCaptureElements(".storybook-component")
+      .capture("default")
+      .capture("default - clicked", actions => {
+        actions.click("label");
+      });
+  });
+  gemini.suite("with reference to form element", suite => {
+    suite
+      .setUrl(
+        "iframe.html?selectedKind=Label&selectedStory=with%20reference%20to%20form%20element"
+      )
+      .setCaptureElements(".storybook-component")
+      .capture("with reference to form element")
+      .capture("with reference to form element - clicked", actions => {
+        actions.click("label");
+      });
+  });
 });
--- packages/label/src/__gemini__/Label.stories-test.js
+++ packages/label/src/__gemini__/Label.stories-test.js
@@ -1,6 +1,16 @@
+import Input from "@hig/input";
 import React from "react";
 import { storiesOf } from "@storybook/react";
 
 import Label from "../index";
 
-storiesOf("Label", module).add("default", () => <Label>Email</Label>);
+storiesOf("Label", module)
+  .add("default", () => <Label>Email</Label>)
+  .add("with reference to form element", () => (
+    <form id="a_form">
+      <Label form="a_form" htmlFor="an_input">
+        Input Field
+      </Label>
+      <Input id="an_input" variant="line" />
+    </form>
+  ));
